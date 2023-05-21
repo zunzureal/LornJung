3f9e324afa24d5a8e56fb04712bd8397299f3198
Punyapat Chanthakhun <57216006+zunzureal@users.noreply.github.com>
2023-05-22 01:56:58 +0700

    Edit by Fluk
    set image background and set Layouit
    
    Co-authored-by: jeagerism<https://github.com/jeagerism>

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import * as Location from 'expo-location';
-import { API_KEY } from './utils/WeatherAPIKey';
-import Weather from './components/Weather';
+import React from "react";
+import { StyleSheet, Text, View, ImageBackground } from "react-native";
+import * as Location from "expo-location";
+import { API_KEY } from "./utils/WeatherAPIKey";
+import Weather from "./components/Weather";
+
+const image = {
+  uri: "https://docs.expo.dev/static/images/tutorial/splash.png",
+};
 
 export default class App extends React.Component {
   state = {
@@ -16,7 +20,7 @@ export default class App extends React.Component {
   componentDidMount() {
     Location.requestForegroundPermissionsAsync()
       .then(({ status }) => {
-        if (status === 'granted') {
+        if (status === "granted") {
           Location.getCurrentPositionAsync()
             .then((position) => {
               this.fetchWeather(
@@ -25,24 +29,24 @@ export default class App extends React.Component {
               );
             })
             .catch((error) => {
-              console.error('Error Getting Weather Conditions:', error);
+              console.error("Error Getting Weather Conditions:", error);
               this.setState({
                 isLoading: false,
-                error: 'Error Getting Weather Conditions',
+                error: "Error Getting Weather Conditions",
               });
             });
         } else {
           this.setState({
             isLoading: false,
-            error: 'Location permission not granted',
+            error: "Location permission not granted",
           });
         }
       })
       .catch((error) => {
-        console.error('Error Getting Location Permission:', error);
+        console.error("Error Getting Location Permission:", error);
         this.setState({
           isLoading: false,
-          error: 'Error Getting Location Permission',
+          error: "Error Getting Location Permission",
         });
       });
   }
@@ -66,46 +70,79 @@ export default class App extends React.Component {
       })
       .catch((error) => {
         console.error(error);
-        this.setState({ isLoading: false, error: 'Error Fetching Weather' });
+        this.setState({ isLoading: false, error: "Error Fetching Weather" });
       });
   };
 
   render() {
-    const { isLoading, temperature, weatherCondition, error, country , name } =
+    const { isLoading, temperature, weatherCondition, error, country, name } =
       this.state;
 
     return (
-      <View style={styles.container}>
-        {isLoading ? (
-          <Text style={styles.loadingText}>Fetching The Weather</Text>
-        ) : error ? (
-          <Text style={styles.errorText}>{error}</Text>
-        ) : (
-          <Weather
-            temperature={temperature}
-            weatherCondition={weatherCondition}
-            country={country}
-            name={name}
-          />
-        )}
-      </View>
+      <ImageBackground source={require("./sun.jpg")} style={styles.image}>
+        <View style={styles.container}>
+          <View style={styles.boxer}>
+            {isLoading ? (
+              <Text style={styles.loadingText}>Fetching The Weather</Text>
+            ) : error ? (
+              <Text style={styles.errorText}>{error}</Text>
+            ) : (
+              <Weather
+                temperature={temperature}
+                weatherCondition={weatherCondition}
+                country={country}
+                name={name}
+              />
+            )}
+          </View>
+
+          <View style={styles.boxer}>
+            {isLoading ? (
+              <Text style={styles.loadingText}>Fetching The Weather</Text>
+            ) : error ? (
+              <Text style={styles.errorText}>{error}</Text>
+            ) : (
+              <Weather
+                temperature={temperature}
+                weatherCondition={weatherCondition}
+                country={country}
+                name={name}
+              />
+            )}
+          </View>
+        </View>
+      </ImageBackground>
     );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
+    flexDirection: "row",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    flex: 1,
+    justifyContent: "center",
   },
   loadingText: {
     fontSize: 20,
-    color: '#000',
+    color: "rgba(236, 153, 95, 1)",
   },
   errorText: {
     fontSize: 20,
-    color: 'red',
+    color: "red",
+  },
+  boxer: {
+    paddingTop: 10,
+    paddingHorizontal: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 1,
+    borderColor: "white",
+    margin: 20,
+    borderRadius: 10,
   },
 });
